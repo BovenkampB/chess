cef781a66734b515c29d98e9a607c5e842b4fb15
bart.bov@gmail.com <bvdbovenkamp@tricode.nl>
2022-10-09 01:27:48 +0200

    Added basic chat msg functionality

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 
-const { Server } = require("socket.io");
+const { Server } = require('socket.io');
 const io = new Server(server);
 
 const hostname = '127.0.0.1';
@@ -11,13 +11,21 @@ const port = 3000;
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
-    // res.send('<h1>Chatten</h1>')
+});
+
+io.on('connection', (socket) => {
+
+    socket.on('chat message', (msg) => {
+       console.log("Message: " + msg);
+    });
+
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
 });
 
 server.listen(3000, hostname,() => {
     console.log("server started on", hostname, port);
 });
 
-io.on('connection', (socket) => {
-    console.log("Gebruiker is verbonden.");
-});
