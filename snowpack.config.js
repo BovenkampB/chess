08c882a1f917d9119ed5ae5c9d3f06078d68d4e2
bart.bov@gmail.com <bvdbovenkamp@tricode.nl>
2022-10-10 00:11:37 +0200

    added snowpack config

---
+++ snowpack.config.js
@@ -0,0 +1,28 @@
+// Snowpack Configuration File
+// See all supported options: https://www.snowpack.dev/reference/configuration
+
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  //Just needed for snowpack build so that it minifies
+  experiments: {
+    optimize: {
+      bundle: true,
+      minify: true,
+    },
+  },
+  mount: {
+    /* ... */
+  },
+  plugins: [
+    /* ... */
+  ],
+  packageOptions: {
+    /* ... */
+  },
+  devOptions: {
+    /* ... */
+  },
+  buildOptions: {
+    /* ... */
+  },
+}
