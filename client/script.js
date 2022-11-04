9f92dfed0a0b8cbf99b22e941f9e6c11809f0222
bart.bov@gmail.com <bvdbovenkamp@tricode.nl>
2022-11-04 16:11:25 +0100

    cleanup unused stuff

--- client/script.js
+++ client/script.js
@@ -2,19 +2,15 @@ import {io} from "socket.io-client";
 
 const form = document.getElementById('form');
 const messageInput = document.getElementById('input');
-// const nameInput = document.getElementById('nameInput');
-const messageBtn = document.getElementById('button');
-
-// let displayName= '';
 
 const socket = io("http://localhost:3000");
 
 socket.on('connect', () => {
-    displayMessage(`You connected with id: ${socket.id} `)
+    displayMessage(`You connected with id: ${socket.id}`, "")
 });
 
-socket.on('receive-message', (msg) => {
-    displayMessage(msg)
+socket.on('receive-message', (msg, sender) => {
+    displayMessage(msg, sender)
 })
 
 form.addEventListener('submit', function(e) {
@@ -22,14 +18,14 @@ form.addEventListener('submit', function(e) {
     let message = messageInput.value;
 
     if (message) {
-        displayMessage(message);
+        displayMessage(message, "you");
         messageInput.value = '';
         socket.emit('send-message', message);
     }
 });
 
-function displayMessage(message) {
+function displayMessage(message, sender) {
     const div = document.createElement('div');
-    div.textContent = message;
+    div.textContent = sender + " : " + message;
     document.getElementById("message-container").append(div);
 }
--- server/server.js
+++ server/server.js
@@ -1,43 +1,25 @@
-// const express = require('express');
-// const app = express();
-// // const http = require('http');
-// var path = require('path');
 require('dotenv').config();
-// const { Server } = require('socket.io');
 
-// const server = http.createServer(app);
-// const io = new Server(server);
 const io = require("socket.io")(process.env.PORT, {
     cors: {
         origin: ['http://localhost:8080'],
     },
 });
 
-// app.use(express.static(path.join(__dirname, 'assets'))); //  "public" off of current is root
-
-// app.get('/', (req, res) => {
-//     res.sendFile(__dirname + '/index.html');
-// });
-
 io.on("connection", socket => {
     console.log(socket.id);
 
     socket.on('send-message', (msg) => {
-        console.log("Message: " + msg);
-        socket.broadcast.emit("receive-message", msg); // everyone gets it but the sender
+        socket.broadcast.emit("receive-message", msg, socket.id); // everyone gets it but the sender
     });
 
     console.log('a user connected');
     socket.on('disconnect', () => {
-        console.log('user disconnected');
+        console.log('user disconnected: '+ socket.id);
     });
 
     socket.on('custom-event', (number, string, obj) => {
         console.log(number, string, obj);
     })
 });
-
-// server.listen(process.env.PORT, process.env.HOSTNAME,() => {
-//     console.log("server started on", process.env.HOSTNAME, process.env.PORT);
-// });
 
