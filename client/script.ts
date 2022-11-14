a5a8ac09e8761990deb1404ec5a62cb81f22ab55
bart.bov@gmail.com <bvdbovenkamp@tricode.nl>
2022-11-14 12:18:02 +0100

    typescript

--- client/script.ts
+++ client/script.ts
@@ -6,46 +6,51 @@ const messageInput = (<HTMLInputElement>document.getElementById('input'));
 
 const socket = io("http://localhost:3000");
 
-socket.on('connect', () => {
-    displayMessage(`You connected with id: ${socket.id}`, "")
-});
 
-socket.on('receive-message', (msg, sender) => {
-    displayMessage(msg, sender)
-})
+export const client = () => {
 
-form.addEventListener('submit', function(e) {
-    e.preventDefault();
-    let message = messageInput.value;
+    socket.on('connect', () => {
+        displayMessage(`You connected with id: ${socket.id}`, "")
+    });
 
-    if (message) {
-        displayMessage(message, "you");
-        messageInput.value = '';
-        socket.emit('send-message', message);
+    socket.on('receive-message', (msg, sender) => {
+        displayMessage(msg, sender)
+    })
+
+    form.addEventListener('submit', function(e) {
+        e.preventDefault();
+        let message = messageInput.value;
+
+        if (message) {
+            displayMessage(message, "you");
+            messageInput.value = '';
+            socket.emit('send-message', message);
+        }
+    });
+
+    function displayMessage(message, sender) {
+        const div = document.createElement('div');
+        div.textContent = sender + " : " + message;
+        document.getElementById("message-container").append(div);
     }
-});
 
-function displayMessage(message, sender) {
-    const div = document.createElement('div');
-    div.textContent = sender + " : " + message;
-    document.getElementById("message-container").append(div);
 }
 
 
-type User = {
+export type User = {
     name: string;
     age: number;
 };
 
-function isAdult(user: User): boolean {
+export function isAdult(user: User): boolean {
     return user.age >= 18;
 }
 
-const justine: User = {
+export const justine: User = {
     name: 'Justine',
     age: 15,
 };
 
-const isJustineAnAdult: boolean = isAdult(justine);
+export const isJustineAnAdult: boolean = isAdult(justine);
 
 
--- server/server.ts
+++ server/server.ts
@@ -6,24 +6,27 @@ const io = require("socket.io")(process.env.PORT, {
     },
 });
 
-io.on("connection", socket => {
-    console.log(socket.id);
+
+export const server = () => {
+    io.on("connection", socket => {
+        console.log(socket.id);
 
 
-    socket.on('set-username', (username) => {
-        //check socket api to set a custom property "username"
+        socket.on('set-username', (username) => {
+            //check socket api to set a custom property "username"
+        });
+
+        socket.on('send-message', (msg) => {
+            socket.broadcast.emit("receive-message", msg, socket.id); // everyone gets it but the sender
+        });
+
+        console.log('a user connected');
+        socket.on('disconnect', () => {
+            console.log('user disconnected: '+ socket.id);
+        });
+
+        socket.on('custom-event', (number, string, obj) => {
+            console.log(number, string, obj);
+        })
     });
-
-    socket.on('send-message', (msg) => {
-        socket.broadcast.emit("receive-message", msg, socket.id); // everyone gets it but the sender
-    });
-
-    console.log('a user connected');
-    socket.on('disconnect', () => {
-        console.log('user disconnected: '+ socket.id);
-    });
-
-    socket.on('custom-event', (number, string, obj) => {
-        console.log(number, string, obj);
-    })
-});
+}
